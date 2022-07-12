faac86495664af5528dc91f1e76b4070dc56f94d
Asim Ali <hi@asimali.net>
2022-07-12 14:02:12 +0000

    dir changed

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ async function handleRequest(request) {
   console.log(path);
   if (!path) {
     const html = await fetch(
-      "https://raw.githubusercontent.com/imasimali/short-url-cf/main/index.html"
+      "https://raw.githubusercontent.com/imasimali/short-url-cf/main/render/index.html"
     );
 
     return new Response(await html.text(), {
@@ -155,7 +155,7 @@ async function handleRequest(request) {
   if (location) {
     if (config.no_ref == "on") {
       let no_ref = await fetch(
-        "https://raw.githubusercontent.com/imasimali/short-url-cf/main/no-ref.html"
+        "https://raw.githubusercontent.com/imasimali/short-url-cf/main/render/no-ref.html"
       );
       no_ref = await no_ref.text();
       no_ref = no_ref.replace(/{Replace}/gm, location);
